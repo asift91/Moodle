87add6a6707bf117047a2c3a7f887b54961f0711
asift91 <asift@italentdigital.com>
2021-01-13 11:59:53 +0530

    added echo statements

--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# create a file for testing in root location 
+# create a file for testing in root location >> /home/azureuser/log.txt
 
-cd ~
-touch test1.txt 
+cd ~ >> /home/azureuser/log.txt
+echo "touch test1.txt" >> /home/azureuser/log.txt
+touch test1.txt >> /home/azureuser/log.txt
